32cb4b01ec9a3515777735574184225f4aa22270
Rich D <richie.donovan62@gmail.com>
2019-02-11 22:17:24 -0600

    Updates

---
+++ precache-manifest.e925e0a44c4da99627db8f23a81309e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "fc006af9e03d261ccfda",
+    "url": "/clickygame/static/js/main.fc006af9.chunk.js"
+  },
+  {
+    "revision": "6cc3fba143227803f814",
+    "url": "/clickygame/static/js/1.6cc3fba1.chunk.js"
+  },
+  {
+    "revision": "fc006af9e03d261ccfda",
+    "url": "/clickygame/static/css/main.b767e2f6.chunk.css"
+  },
+  {
+    "revision": "2926dee165aff32bcca304a1c8b4ad31",
+    "url": "/clickygame/index.html"
+  }
+];
